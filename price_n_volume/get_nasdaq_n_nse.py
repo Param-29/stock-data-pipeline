c5184614572e58504352999fbb7038c9901130b6
Param Mirani <param.mirani1999@gmail.com>
2023-04-16 17:09:25 +0530

    commit-one-file-direcotry-changed-for-gcs prefect to start

--- price_n_volume/get_nasdaq_n_nse.py
+++ price_n_volume/get_nasdaq_n_nse.py
@@ -1,10 +1,29 @@
 import requests
 import pandas as pd 
 import os
+import json
+import time
 # Replace YOUR_API_KEY with your actual API key
 # number_of_rows = 400
 
+
 TESTING = False
+
+def get_api_key():
+    f = open('../api_key.json')
+    try:
+        data = json.load(f)
+    except Exception as e:
+        print(f'Error: json read\n {e}')
+        return "-1"
+    
+    if "alphavantage" in data.keys():
+        print(f'Key found; returning key')
+        return data["alphavantage"]
+    else:
+        print('Key not found; file should be following\n\t {"alphavantage" : "your_key"}')
+        return "-1"
+
 
 def get_and_preprocess(symbol, api_key, colmn_rename):
     # Construct the API endpoint URL
@@ -12,16 +31,26 @@ def get_and_preprocess(symbol, api_key, colmn_rename):
 
     # Make a GET request to the API endpoint
     response = requests.get(url)
-
+    # 5 api calls for every 5 min, hence need to sleep 1m every api call :)) 
+    print(f'Sleeping for 1min....')
+    time.sleep(60)
     # Check if the request was successful
     if response.status_code == 200:
         # Get the close data from the response JSON
-        data = response.json()['Time Series (Daily)']
+        try:
+            data = response.json()['Time Series (Daily)']
+        except Exception as e:
+            print(f'Data for {symbol} not found\n error = {e}')
+            print(f'Data = {response.json()}')
+            df = pd.DataFrame()
+            return df
         close_data = {date: float(values['4. close']) for date, values in data.items()}
         print(f'data recieved from api {len(close_data)} for {symbol}')
         # print(close_data)
     else:
         print(f'Request failed with status code {response.status_code} for {symbol}')
+        df = pd.DataFrame()
+        return df
 
 
     df_tmp = pd.DataFrame(data) \
@@ -43,23 +72,47 @@ def write_data_to_file(df, company):
 
     min_year = min(df.date).year
     max_year = max(df.date).year
-    for year in range (min_year, max_year+1):
+    
+    # create historic folder 
+    for year in range (min_year, max_year):
         df_year = df[df['date'].dt.year == year]
         # include = df[df['Date'].dt.year == year]
         print(f'{company}: {year}: {len(df_year)}')
-        path = f'data/price_n_volume/{year}/'
-        isExist = os.path.exists(path)
+        path_historic = f'data/price_n_volume/historic/{year}/'
+        isExist = os.path.exists(path_historic)
         if not isExist:
 
            # Create a new directory because it does not exist
-           os.makedirs(path)
-           print("The new directory is created!")
+           os.makedirs(path_historic)
+           print(f"The new directory is created! {path_historic}")
 
-        df_year.to_parquet(f'{path}/{company}.parquet')
+        df_year.to_parquet(f'{path_historic}/{company}.parquet', index=False)
         # df_year.to_csv(f'{path}/{company}.csv')
+    
+    # create recent folder, this year and last year;
+    # hdr = False  if os.path.isfile('filename.csv') else True
+    # df.to_csv('filename.csv', mode='a', header=hdr)
+    for year in range (max_year -1, max_year + 1):
+        df_year = df[df['date'].dt.year == year]
+        for date_ in df_year.date.unique():
+            df_date = df_year[df_year['date'] == date_]
+            path_recent = f'data/price_n_volume/recent/'
+            isExist = os.path.exists(path_recent)
+            if not isExist:
+                os.makedirs(path_recent)
+                print(f"The new directory is created! {path_recent}")
+            
+            date_csv_path = f'{path_recent}/{date_}.csv'
+            hdr = False  if os.path.isfile(date_csv_path) else True
+            df_date.to_csv(date_csv_path, mode='a', header=hdr, index=False)
+
 
 if __name__=="__main__":
-    api_key = 'xD'
+    api_key = get_api_key()
+    
+    if api_key == "-1":
+        exit(1)
+    
     colmn_rename = {
         'index': 'date',
         '1. open':'open',
@@ -77,12 +130,13 @@ if __name__=="__main__":
     temp = open(filename,'r').read().split('\n')
     print(f'All files listed in file: {temp}')
 
-    for i in range(5):
+    for i in range(10):
         symbol = temp[i]
 
         df_all = get_and_preprocess(symbol, api_key, colmn_rename)
 
-        write_data_to_file(df_all, symbol)
+        if not df_all.empty:
+            write_data_to_file(df_all, symbol)
 
 
 
---
+++ price_n_volume/push_to_gcs.py
@@ -0,0 +1,7 @@
+# Purpose 
+# 1. Push Files to gcs bucket using gcs
+# 2. Push recent files to BigQuery using bigquery 
+# 3. Create a script that would run everyday to update data into 
+#     1. BigQuery
+#     2. GCS
+
